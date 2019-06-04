299529f0c8db44bee4e3a1b5f2b99dee87ade5b4
Marcos Lehmkuhl <m.v.lehmkuhl@gmail.com>
2019-06-04 02:32:05 +0000

    feat: load components dynamically.

--- src/Controller/Storybook.php
+++ src/Controller/Storybook.php
@@ -3,16 +3,29 @@
 namespace Drupal\twig_storybook\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Controller to render stories
  */
 class Storybook extends ControllerBase {
-
   public function build() {
+    $themeManager = \Drupal::service('theme.manager');
+    $activeTheme = $themeManager->getActiveTheme();
+    $activeThemePath = $activeTheme->getPath();
+    $activeThemeName = $activeTheme->getName();
+    $fileName = $activeThemeName . '.stories.yml';
+    $filePath = DRUPAL_ROOT . '/' . $activeThemePath . '/' . $fileName;
+
+    $file_contents = file_get_contents($filePath);
+    $components = Yaml::parse($file_contents);
+
     return [
       '#theme' => 'storybook_template',
-      '#variables' => []
+      '#variables' => [
+        'activeTheme' => $activeThemeName,
+        'components' => $components
+      ]
     ];
   }
 }
