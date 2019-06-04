a089df1c124a8cb0f875c584605a5d76ab4cf51e
Marcos Lehmkuhl <m.v.lehmkuhl@gmail.com>
2019-06-04 05:12:05 +0000

    feat: add basic generated content based on deve_generate.

--- src/Controller/Storybook.php
+++ src/Controller/Storybook.php
@@ -4,11 +4,53 @@ namespace Drupal\twig_storybook\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\Yaml\Yaml;
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
 
 /**
  * Controller to render stories
  */
 class Storybook extends ControllerBase {
+
+  protected function generateContent($component) {
+    $entityManagerStorage = \Drupal::service('entity_type.manager')
+      ->getStorage($component['type']);
+
+    $entity = $entityManagerStorage->create([
+      'nid' => NULL,
+      'type' => $component['bundle'],
+
+      // make it configurable
+      'title' => Random::name(),
+      'status' => TRUE,
+    ]);
+
+    $instances = entity_load_multiple_by_properties('field_config',[
+      'entity_type' => $entity->getEntityType()->id(),
+      'bundle' => $entity->bundle()
+    ]);
+
+    foreach ($instances as $instance) {
+      $field_storage = $instance->getFieldStorageDefinition();
+      $max = $cardinality = $field_storage->getCardinality();
+      if ($cardinality == FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED) {
+        // Just an arbitrary number for 'unlimited'
+        $max = rand(1, 3);
+      }
+      $field_name = $field_storage->getName();
+      $entity->$field_name->generateSampleItems($max);
+    }
+
+    foreach($component['data'] as $key => $value) {
+      if (!$entity->hasField($value)) {
+        throw new \Exception('Entity has no field');
+      }
+      $component['data'][$key] = $entity->get($value)->view();
+    }
+
+    return $component;
+  }
+
   public function build() {
     $themeManager = \Drupal::service('theme.manager');
     $activeTheme = $themeManager->getActiveTheme();
@@ -19,6 +61,12 @@ class Storybook extends ControllerBase {
 
     $file_contents = file_get_contents($filePath);
     $components = Yaml::parse($file_contents);
+
+    foreach($components as $name => $component) {
+      if(!empty($component['type'])) {
+        $components[$name] = $this->generateContent($component);
+      }
+    }
 
     return [
       '#theme' => 'storybook_template',
